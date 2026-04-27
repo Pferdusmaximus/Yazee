ddb718f9f0fd2b4885450d46ce5c4411686b642b
Pferdusmaximus <nilsbraeunlein@gmail.com>
2026-04-27 12:55:46 +0200

    fenster

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pygame, player, game, dice, cup, scorecard, rules
 
+pygame.init()
+window = pygame.display.set_mode((1200, 700))
+
+window.fill((74, 154, 69))  # Rot
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
